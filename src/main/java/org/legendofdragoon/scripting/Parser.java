feae7634fb225036bbe8224a4887284a6612d4ac
U-DESKTOP-NEKEHIK\corey <lordmonoxide@gmail.com>
2023-07-26 22:39:38 -0300

    Start of ASMifying output

--- src/main/java/org/legendofdragoon/scripting/Parser.java
+++ src/main/java/org/legendofdragoon/scripting/Parser.java
@@ -61,337 +61,81 @@ public class Parser {
       }
 
       switch(callbackIndex) {
-        case 0 -> lines.put(state.opOffset(), "pause;");
+        case 0 -> lines.put(state.opOffset(), "yield");
+        case 1 -> lines.put(state.opOffset(), "rewind");
+        case 2 -> lines.put(state.opOffset(), "wait %s".formatted(state.getParam(0)));
+        case 3 -> lines.put(state.opOffset(), "comp_wait %s %s %s".formatted(getOperator(parentParam), state.getParam(0), state.getParam(1)));
+        case 4 -> lines.put(state.opOffset(), "comp_wait %s 0 %s".formatted(getOperator(parentParam), state.getParam(0)));
+        case 8 -> lines.put(state.opOffset(), "move %s %s".formatted(state.getParam(1), state.getParam(0)));
+        case 9 -> lines.put(state.opOffset(), "swap_broken %s %s".formatted(state.getParam(0), state.getParam(1)));
+        case 10 -> lines.put(state.opOffset(), "memcpy %s %s %s".formatted(state.getParam(2), state.getParam(1), state.getParam(0)));
+        case 12 -> lines.put(state.opOffset(), "move %s 0".formatted(state.getParam(0)));
+        case 16 -> lines.put(state.opOffset(), "and %s %s".formatted(state.getParam(1), state.getParam(0)));
+        case 17 -> lines.put(state.opOffset(), "or %s %s".formatted(state.getParam(1), state.getParam(0)));
+        case 18 -> lines.put(state.opOffset(), "xor %s %s".formatted(state.getParam(1), state.getParam(0)));
+        case 19 -> lines.put(state.opOffset(), "andor %s %s %s".formatted(state.getParam(2), state.getParam(0), state.getParam(1)));
+        case 20 -> lines.put(state.opOffset(), "not %s".formatted(state.getParam(0)));
+        case 21 -> lines.put(state.opOffset(), "shl %s %s".formatted(state.getParam(1), state.getParam(0)));
+        case 22 -> lines.put(state.opOffset(), "shr %s %s".formatted(state.getParam(1), state.getParam(0)));
+        case 24 -> lines.put(state.opOffset(), "add %s %s".formatted(state.getParam(1), state.getParam(0)));
+        case 25 -> lines.put(state.opOffset(), "sub %s %s".formatted(state.getParam(1), state.getParam(0)));
+        case 26 -> lines.put(state.opOffset(), "sub_rev = %s %s".formatted(state.getParam(1), state.getParam(0)));
+        case 27 -> lines.put(state.opOffset(), "incr %s".formatted(state.getParam(0)));
+        case 28 -> lines.put(state.opOffset(), "decr %s".formatted(state.getParam(0)));
+        case 29 -> lines.put(state.opOffset(), "neg %s".formatted(state.getParam(0)));
+        case 30 -> lines.put(state.opOffset(), "abs %s".formatted(state.getParam(0)));
+        case 32 -> lines.put(state.opOffset(), "mul %s %s".formatted(state.getParam(1), state.getParam(0)));
+        case 33 -> lines.put(state.opOffset(), "div %s %s".formatted(state.getParam(1), state.getParam(0)));
+        case 34 -> lines.put(state.opOffset(), "div_rev %s %s".formatted(state.getParam(1), state.getParam(0)));
+        case 35, 43 -> lines.put(state.opOffset(), "mod %s %s".formatted(state.getParam(1), state.getParam(0)));
+        case 36, 44 -> lines.put(state.opOffset(), "mod_rev %s %s".formatted(state.getParam(1), state.getParam(0)));
+        case 40 -> lines.put(state.opOffset(), "unk_40 %s %s".formatted(state.getParam(1), state.getParam(0)));
+        case 41 -> lines.put(state.opOffset(), "unk_41 %s %s".formatted(state.getParam(1), state.getParam(0)));
+        case 42 -> lines.put(state.opOffset(), "unk_42 %s %s".formatted(state.getParam(1), state.getParam(0)));
+        case 48 -> lines.put(state.opOffset(), "sqrt %s %s".formatted(state.getParam(1), state.getParam(0)));
+        case 49 -> lines.put(state.opOffset(), "rand %s %s".formatted(state.getParam(1), state.getParam(0)));
+        case 50 -> lines.put(state.opOffset(), "sin_12 %s %s".formatted(state.getParam(1), state.getParam(0)));
+        case 51 -> lines.put(state.opOffset(), "cos_12 %s %s".formatted(state.getParam(1), state.getParam(0)));
+        case 52 -> lines.put(state.opOffset(), "atan2_12 %s %s %s".formatted(state.getParam(2), state.getParam(0), state.getParam(1)));
 
-        case 1 -> lines.put(
-          state.opOffset(),
-          """
-          rewind;
-          pause;"""
-        );
-
-        case 2 ->
-          lines.put(
-            state.opOffset(),
-            """
-            // Wait for n frames
-            if(%s != 0) {
-              %s--;
-              // Repeat this same if statement next frame
-              rewind;
-              pause;
-            }""".formatted(state.getParam(0), state.getParam(0))
-          );
-
-        case 3 -> {
-          final String operand = switch(parentParam) {
-            case 0 -> "<=";
-            case 1 -> "<";
-            case 2 -> "==";
-            case 3 -> "!=";
-            case 4 -> ">";
-            case 5 -> ">=";
-            case 6 -> "&";
-            case 7 -> "!&";
-            default -> "//TODO <invalid operand>";
-          };
-
-          lines.put(
-            state.opOffset(),
-            """
-            if(!(%s %s %s)) {
-              rewind;
-              pause;
-            }""".formatted(state.getParam(0), operand, state.getParam(1))
-          );
+        case 56 -> {
+          final String[] params = new String[state.getParamCount()];
+          Arrays.setAll(params, state::getParam);
+          lines.put(state.opOffset(), "call %d (%s)".formatted(parentParam, String.join(", ", params)));
         }
 
-        case 4 -> {
-          final String operand = switch(parentParam) {
-            case 0 -> "<=";
-            case 1 -> "<";
-            case 2 -> "==";
-            case 3 -> "!=";
-            case 4 -> ">";
-            case 5 -> ">=";
-            case 6 -> "&";
-            case 7 -> "!&";
-            default -> "//TODO <invalid operand>";
-          };
-
-          lines.put(
-            state.opOffset(),
-            """
-            if(!(0 %s %s)) {
-              rewind;
-              pause;
-            }""".formatted(operand, state.getParam(0))
-          );
-        }
-
-        case 8 -> // Move
-          lines.put(state.opOffset(), "%s = %s;".formatted(state.getParam(1), state.getParam(0)));
-
-        case 9 -> // Broken swap
-          lines.put(state.opOffset(), """
-          { // Broken swapScript 
-            tmp = %1$s;
-            %2$s = tmp;
-            %1$s = tmp;
-          }""".formatted(state.getParam(0), state.getParam(1)));
-
-        case 10 -> // memcpy
-          lines.put(state.opOffset(), "memcpy(%s, %s, %s); // dst, src, size".formatted(state.getParam(2), state.getParam(1), state.getParam(0)));
-
-        case 12 -> // Set 0
-          lines.put(state.opOffset(), "%s = 0;".formatted(state.getParam(0)));
-
-        case 16 -> // And
-          lines.put(state.opOffset(), "%s &= %s;".formatted(state.getParam(1), state.getParam(0)));
-
-        case 17 -> // Or
-          lines.put(state.opOffset(), "%s |= %s;".formatted(state.getParam(1), state.getParam(0)));
-
-        case 18 -> // Xor
-          lines.put(state.opOffset(), "%s ^= %s;".formatted(state.getParam(1), state.getParam(0)));
-
-        case 19 -> // And+Or
-          lines.put(state.opOffset(), "%s = %s & %s | %s;".formatted(state.getParam(2), state.getParam(2), state.getParam(0), state.getParam(1)));
-
-        case 20 -> // Not
-          lines.put(state.opOffset(), "%s = ~%s;".formatted(state.getParam(0), state.getParam(0)));
-
-        case 21 -> // Shift left
-          lines.put(state.opOffset(), "%s <<= %s;".formatted(state.getParam(1), state.getParam(0)));
-
-        case 22 -> // Shift right
-          lines.put(state.opOffset(), "%s >>= %s;".formatted(state.getParam(1), state.getParam(0)));
-
-        case 24 -> // Add
-          lines.put(state.opOffset(), "%s += %s;".formatted(state.getParam(1), state.getParam(0)));
-
-        case 25 -> // Sub
-          lines.put(state.opOffset(), "%s -= %s;".formatted(state.getParam(1), state.getParam(0)));
-
-        case 26 -> // Sub2
-          lines.put(state.opOffset(), "%s = %s - %s;".formatted(state.getParam(1), state.getParam(0), state.getParam(1)));
-
-        case 27 -> // Incr
-          lines.put(state.opOffset(), "%s++;".formatted(state.getParam(0)));
-
-        case 28 -> // Decr
-          lines.put(state.opOffset(), "%s--;".formatted(state.getParam(0)));
-
-        case 29 -> // Neg
-          lines.put(state.opOffset(), "%s = -%s;".formatted(state.getParam(0), state.getParam(0)));
-
-        case 30 -> // Abs
-          lines.put(state.opOffset(), "%s = |%s|;".formatted(state.getParam(0), state.getParam(0)));
-
-        case 32 -> // Mul
-          lines.put(state.opOffset(), "%s *= %s;".formatted(state.getParam(1), state.getParam(0)));
-
-        case 33 -> // Div
-          lines.put(state.opOffset(), "%s /= %s;".formatted(state.getParam(1), state.getParam(0)));
-
-        case 34 -> // Div2
-          lines.put(state.opOffset(), "%s = %s / %s;".formatted(state.getParam(1), state.getParam(0), state.getParam(1)));
-
-        case 35, 43 -> // Mod
-          lines.put(state.opOffset(), "%s %%= %s;".formatted(state.getParam(1), state.getParam(0)));
-
-        case 36, 44 -> // Mod2
-          lines.put(state.opOffset(), "%s = %s %% %s;".formatted(state.getParam(1), state.getParam(0), state.getParam(1)));
-
-        case 40 -> // ?
-          lines.put(state.opOffset(), "%s = ((%s >> 4) / (%s >> 4)) >> 4;".formatted(state.getParam(1), state.getParam(1), state.getParam(0)));
-
-        case 41 -> // ?
-          lines.put(state.opOffset(), "%s = (%s << 4) * %s << 8;".formatted(state.getParam(1), state.getParam(1), state.getParam(0)));
-
-        case 42 -> // ?
-          lines.put(state.opOffset(), "%s = (%s << 4) * %s << 8;".formatted(state.getParam(1), state.getParam(0), state.getParam(1)));
-
-        case 48 -> // Sqrt0
-          lines.put(state.opOffset(), "%s = sqrt(%s);".formatted(state.getParam(1), state.getParam(0)));
-
-        case 49 -> // Rand
-          lines.put(state.opOffset(), "%s = rand(%s);".formatted(state.getParam(1), state.getParam(0)));
-
-        case 50 -> // Sin12
-          lines.put(state.opOffset(), "%s = sin(%s); // 12-bit fixed-point decimal".formatted(state.getParam(1), state.getParam(0)));
-
-        case 51 -> // Cos12
-          lines.put(state.opOffset(), "%s = cos(%s); // 12-bit fixed-point decimal".formatted(state.getParam(1), state.getParam(0)));
-
-        case 52 -> // Ratan2
-          lines.put(state.opOffset(), "%s = ratan2(%s, %s); // 12-bit fixed-point decimal".formatted(state.getParam(2), state.getParam(0), state.getParam(1)));
-
-        case 56 -> { // Subfunc
-          switch(parentParam) {
-            case 5 -> { // Read script flag
-              lines.put(
-                state.opOffset(),
-                """
-                flag = *(%s);
-                index = flag >>> 5; // Bitset index
-                shift = flag & 0x1f; // Bit number
-                %s = scriptFlags2[index] & (1 << shift); // Read a script flag""".formatted(state.getParam(0), state.getParam(1))
-              );
-            }
-
-            case 195 -> lines.put(state.opOffset(), "%s = ((uint*)0x800be358)[%s] | ((uint*)0x800bdf38)[%s]; // unknown".formatted(state.getParam(1), state.getParam(0), state.getParam(0)));
-
-            default -> {
-              final String[] params = new String[state.getParamCount()];
-              Arrays.setAll(params, state::getParam);
-
-              lines.put(state.opOffset(), "subfunc(%d)(%s)".formatted(parentParam, String.join(", ", params)));
-            }
-          }
-        }
-
-        case 64 -> // Jump
-          lines.put(state.opOffset(), "this.jump(%s);".formatted(state.getParam(0)));
-
-        case 65 -> { // Jump cmp
-          final String operand = switch(parentParam) {
-            case 0 -> "<=";
-            case 1 -> "<";
-            case 2 -> "==";
-            case 3 -> "!=";
-            case 4 -> ">";
-            case 5 -> ">=";
-            case 6 -> "&";
-            case 7 -> "!&";
-            default -> "//TODO <invalid operand>";
-          };
-
-          lines.put(
-            state.opOffset(),
-            """
-            if(%s %s %s) {
-              this.jump(%s);
-            }""".formatted(state.getParam(0), operand, state.getParam(1), state.getParam(2))
-          );
-        }
-
-        case 66 -> { // Jump cmp 0
-          final String operand = switch(parentParam) {
-            case 0 -> "<=";
-            case 1 -> "<";
-            case 2 -> "==";
-            case 3 -> "!=";
-            case 4 -> ">";
-            case 5 -> ">=";
-            case 6 -> "&";
-            case 7 -> "!&";
-            default -> "//TODO <invalid operand>";
-          };
-
-          lines.put(
-            state.opOffset(),
-            """
-            if(0 %s %s) {
-              this.jump(%s);
-            }""".formatted(operand, state.getParam(0), state.getParam(1)));
-        }
-
-        case 67 ->
-          lines.put(
-            state.opOffset(),
-            """
-            %s--; // while loop
-            if(%s != 0) {
-              this.jump(%s);
-            }""".formatted(state.getParam(0), state.getParam(0), state.getParam(1))
-          );
-
-        case 68 -> lines.put(state.opOffset(), "this.jump(%s[%s[%s]])".formatted(state.getParam(1), state.getParam(1), state.getParam(0)));
-
-        case 72 -> { // Jump and link
-          lines.put(state.opOffset(), "gosub %s;".formatted(state.getParam(0)));
+        case 64 -> lines.put(state.opOffset(), "jump %s".formatted(state.getParam(0)));
+        case 65 -> lines.put(state.opOffset(), "comp_jump %s %s %s %s".formatted(getOperator(parentParam), state.getParam(0), state.getParam(1), state.getParam(2)));
+        case 66 -> lines.put(state.opOffset(), "comp_jump %s 0 %s %s".formatted(getOperator(parentParam), state.getParam(0), state.getParam(1)));
+        case 67 -> lines.put(state.opOffset(), "while %s %s".formatted(state.getParam(0), state.getParam(1)));
+        case 68 -> lines.put(state.opOffset(), "jump_table %s %s".formatted(state.getParam(1), state.getParam(0)));
+        case 72 -> {
+          lines.put(state.opOffset(), "gosub %s".formatted(state.getParam(0)));
 
           try {
             functions.add(Integer.parseInt(state.getParam(0).substring(2), 16));
           } catch(final NumberFormatException ignored) { }
         }
 
-        case 73 -> // Return
-          lines.put(state.opOffset(), "return;");
-
-        case 74 -> { // Jump and link table
-          lines.put(state.opOffset(), "gosub %s + %s[%s] * 4;".formatted(state.getParam(1), state.getParam(1), state.getParam(0)));
+        case 73 -> lines.put(state.opOffset(), "return");
+        case 74 -> {
+          lines.put(state.opOffset(), "gosub_table %s %s".formatted(state.getParam(1), state.getParam(0)));
 
           try {
             functions.add(Integer.parseInt(state.getParam(0).substring(2), 16));
           } catch(final NumberFormatException ignored) { }
         }
 
-        case 80 ->
-          lines.put(
-            state.opOffset(),
-            """
-            deallocate;
-            pause;
-            rewind;"""
-          );
+        case 80 -> lines.put(state.opOffset(), "deallocate");
+        case 82 -> lines.put(state.opOffset(), "deallocate");
+        case 83 -> lines.put(state.opOffset(), "deallocate %s".formatted(state.getParam(0)));
+        case 86 -> lines.put(state.opOffset(), "fork %s %s %s".formatted(state.getParam(0), state.getParam(1), state.getParam(2)));
+        case 87 -> lines.put(state.opOffset(), "fork_reenter %s %s %s".formatted(state.getParam(0), state.getParam(1), state.getParam(2)));
+        case 88 -> lines.put(state.opOffset(), "consume");
+        case 96, 97, 98 -> lines.put(state.opOffset(), "noop");
+        case 99 -> lines.put(state.opOffset(), "depth %s".formatted(state.getParam(0)));
 
-        case 82 ->
-          lines.put(
-            state.opOffset(),
-            """
-            deallocate children;
-            deallocate;
-            pause;
-            rewind;"""
-          );
-
-        case 83 ->
-          lines.put(
-            state.opOffset(),
-            """
-            deallocate %s;
-            if(%s is self) {
-              pause;
-              rewind;
-            }""".formatted(state.getParam(0), state.getParam(0))
-          );
-
-        case 86 ->
-          lines.put(
-            state.opOffset(),
-            """
-            // Fork and jump script that was forked
-            state[%1$s].fork();
-            state[%1$s].jump(*%2$s);
-            state[%1$s].storage[32] = %3$s""".formatted(state.getParam(0), state.getParam(1), state.getParam(2))
-          );
-
-        case 87 ->
-          lines.put(
-            state.opOffset(),
-            """
-            // Fork and re-enter script that was forked
-            state[%1$s].fork();
-            state[%1$s].jump(entry @ *%2$s);
-            state[%1$s].storage[32] = %3$s""".formatted(state.getParam(0), state.getParam(1), state.getParam(2))
-          );
-
-        case 88 ->
-          lines.put(
-            state.opOffset(),
-            """
-            consume child;
-            deallocate child;
-            jump to child offset;"""
-          );
-
-        case 96, 97, 98 -> lines.put(state.opOffset(), "// noop");
-
-        case 99 -> lines.put(state.opOffset(), "%s = this.stackDepth".formatted(state.getParam(0)));
-
-        case 5, 6, 7, 11, 13, 14, 15 -> lines.put(state.opOffset(), "// not implemented - pause and rewind");
+        case 5, 6, 7, 11, 13, 14, 15 -> lines.put(state.opOffset(), "rewind");
 
         default -> {
           lines.put(state.opOffset(), "0x%x //TODO Unknown function %d, data?".formatted(parentCommand, callbackIndex));
@@ -422,6 +166,20 @@ public class Parser {
         }
       }
     }
+  }
+
+  private static String getOperator(final int index) {
+    return switch(index) {
+      case 0 -> "<=";
+      case 1 -> "<";
+      case 2 -> "==";
+      case 3 -> "!=";
+      case 4 -> ">";
+      case 5 -> ">=";
+      case 6 -> "&";
+      case 7 -> "!&";
+      default -> "//TODO <invalid operand>";
+    };
   }
 
   private static final String[] functions = {
