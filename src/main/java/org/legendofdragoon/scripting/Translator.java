d1646e84633b8f192bdfbe52027c6c082919039e
Corey Frenette <lordmonoxide@gmail.com>
2023-08-29 17:48:31 -0300

    Handle table labels

--- src/main/java/org/legendofdragoon/scripting/Translator.java
+++ src/main/java/org/legendofdragoon/scripting/Translator.java
@@ -87,7 +87,19 @@ public class Translator {
 
   private String buildParam(final Op op, final Param param) {
     if(param.label != null) {
-      return ':' + param.label;
+      final String label = ':' + param.label;
+
+      return switch(param.type) {
+        case INLINE_2 -> "inl[%s[stor[%d]]]".formatted(label, param.rawValues[0] >> 16 & 0xff);
+        case INLINE_3 -> "inl[%1$s[%1$s[stor[%2$d]]]]".formatted(label, param.rawValues[0] >> 16 & 0xff);
+        case INLINE_4 -> "inl[%1$s[%1$s[stor[%2$d]] + stor[%3$d]]]".formatted(label, param.rawValues[1] & 0xff, param.rawValues[1] >> 8 & 0xff);
+        case INLINE_6 -> "inl[%1$s[%1$s[stor[%2$d]] + %3$d]]".formatted(label, param.rawValues[1] & 0xff, param.rawValues[1] >> 8 & 0xff);
+        case _12 -> throw new RuntimeException("Param type 0x12 not yet supported");
+        case _15 -> throw new RuntimeException("Param type 0x15 not yet supported");
+        case _16 -> throw new RuntimeException("Param type 0x16 not yet supported");
+        case INLINE_7 -> "inl[%1$s[%1$s[%2$d] + %3$d]]".formatted(label, param.rawValues[1] & 0xff, param.rawValues[1] >> 8 & 0xff);
+        default -> ':' + param.label;
+      };
     }
 
     return switch(param.type) {
@@ -103,7 +115,7 @@ public class Translator {
       case INLINE_1 -> "inl[0x%x]".formatted(op.address + (short)param.rawValues[0] * 4);
       case INLINE_2 -> "inl[0x%x[stor[%d]]]".formatted(op.address + (short)param.rawValues[0] * 4, param.rawValues[0] >> 16 & 0xff);
       case INLINE_3 -> "inl[0x%1$x[0x%1$x[stor[%2$d]]]]".formatted(op.address + (short)param.rawValues[0] * 4, param.rawValues[0] >> 16 & 0xff);
-      case INLINE_4 -> "inl[%1$x[%1$x[stor[%2$d]] + stor[%3$d]]]".formatted(op.address, param.rawValues[1] & 0xff, param.rawValues[1] >> 8 & 0xff);
+      case INLINE_4 -> "inl[0x%1$x[0x%1$x[stor[%2$d]] + stor[%3$d]]]".formatted(op.address, param.rawValues[1] & 0xff, param.rawValues[1] >> 8 & 0xff);
       case OTHER_STORAGE -> "script[stor[%d]].stor[%d]".formatted(param.rawValues[0] & 0xff, param.rawValues[0] >> 8 & 0xff + param.rawValues[0] >> 16 & 0xff);
       case GAMEVAR_3 -> "var[%d + %d]".formatted(param.rawValues[0] & 0xff, param.rawValues[0] >> 8 & 0xff);
       case GAMEVAR_ARRAY_3 -> "var[%d][%d]".formatted(param.rawValues[0] & 0xff, param.rawValues[0] >> 8 & 0xff);
@@ -114,7 +126,7 @@ public class Translator {
       case INLINE_6 -> "inl[0x%1$x[0x%1$x[stor[%2$d]] + %3$d]]".formatted(op.address, param.rawValues[1] & 0xff, param.rawValues[1] >> 8 & 0xff);
       case _15 -> throw new RuntimeException("Param type 0x15 not yet supported");
       case _16 -> throw new RuntimeException("Param type 0x16 not yet supported");
-      case INLINE_7 -> "inl[%1$x[%1$x[%2$d] + %3$d]]".formatted(op.address, param.rawValues[1] & 0xff, param.rawValues[1] >> 8 & 0xff);
+      case INLINE_7 -> "inl[0x%1$x[0x%1$x[%2$d] + %3$d]]".formatted(op.address, param.rawValues[1] & 0xff, param.rawValues[1] >> 8 & 0xff);
     };
   }
 }
